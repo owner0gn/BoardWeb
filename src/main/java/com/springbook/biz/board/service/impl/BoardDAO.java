250991123035ea27e5cafd36c512298a3ee1835a
owner0gn <owner0gn@gmail.com>
2019-02-12 16:19:51 +0900

    mybatis setting

--- src/main/java/com/springbook/biz/board/service/impl/BoardDAO.java
+++ src/main/java/com/springbook/biz/board/service/impl/BoardDAO.java
@@ -2,6 +2,7 @@ package com.springbook.biz.board.service.impl;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import com.springbook.biz.board.service.BoardVO;
@@ -9,20 +10,21 @@ import com.springbook.biz.board.service.BoardVO;
 @Repository("boardDAO")
 public class BoardDAO {
 	
-	private BoardVO boardVO;
+	private SqlSession sqlSession;
+	
 	public void insertBoard(BoardVO vo) {
-		
-	};
+		sqlSession.insert("BoardDAO.insertBoard", vo);
+	}
 	public void updateBoard(BoardVO vo) {
-		
-	};
+		sqlSession.update("BoardDAO.updateBoard", vo);
+	}
 	public void deleteBoard(BoardVO vo) {
-		
-	};
+		sqlSession.delete("BoardDAO.deleteBoard", vo);
+	}
 	public BoardVO getBoard(BoardVO vo) {
-		return null;
-	};
+		return (BoardVO) sqlSession.selectOne("BoardDAO.getBoard", vo);
+	}
 	public List<BoardVO> getBoardList(BoardVO vo){
-		return null;
+		return sqlSession.selectList("BoardDAO.getBoardList", vo);
 	};
 }
